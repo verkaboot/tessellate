703a7caafe6dfa5e9e5b8ccb55afc67f691edae2
bokubeam <bokubeam@gmail.com>
2024-09-04 10:39:24 -0600

    Add zoom

--- src/camera.rs
+++ src/camera.rs
@@ -5,15 +5,28 @@ use crate::canvas::SIZE;
 
 pub(super) fn plugin(app: &mut App) {
     app.add_plugins(InputManagerPlugin::<CameraMovement>::default())
+        .insert_resource(ClashStrategy::PrioritizeLongest)
         .add_systems(Startup, setup)
-        .add_systems(Update, (pan, zoom));
+        .add_systems(Update, (pan, zoom, zoom_scroll));
 }
 
 pub fn setup(mut commands: Commands) {
+    use CameraMovement::*;
+
     let input_map = InputMap::default()
-        .with(CameraMovement::ActivatePan, MouseButton::Right)
-        .with_axis(CameraMovement::Zoom, MouseScrollAxis::Y)
-        .with_dual_axis(CameraMovement::Pan, MouseMove::default());
+        .with(ZoomModifier, ModifierKey::Alt)
+        .with_dual_axis(
+            Pan,
+            DualAxislikeChord::new(MouseButton::Right, MouseMove::default()),
+        )
+        .with_axis(
+            Zoom,
+            AxislikeChord::new(
+                ButtonlikeChord::from_single(MouseButton::Right).with(ModifierKey::Alt),
+                MouseMoveAxis::Y,
+            ),
+        )
+        .with_axis(ZoomWheel, MouseScrollAxis::Y);
 
     commands.spawn((
         Name::new("Camera"),
@@ -32,41 +45,51 @@ pub fn setup(mut commands: Commands) {
 
 #[derive(Clone, Debug, Copy, PartialEq, Eq, Hash, Reflect)]
 enum CameraMovement {
-    ActivatePan,
     Pan,
     Zoom,
+    ZoomModifier,
+    ZoomWheel,
 }
 
 impl Actionlike for CameraMovement {
     fn input_control_kind(&self) -> InputControlKind {
         match self {
-            CameraMovement::ActivatePan => InputControlKind::Button,
             CameraMovement::Pan => InputControlKind::DualAxis,
             CameraMovement::Zoom => InputControlKind::Axis,
+            CameraMovement::ZoomModifier => InputControlKind::Button,
+            CameraMovement::ZoomWheel => InputControlKind::Axis,
         }
     }
 }
 
 fn pan(mut query: Query<(&mut Transform, &ActionState<CameraMovement>), With<Camera2d>>) {
-    const CAMERA_PAN_RATE: f32 = 1.0;
+    const CAMERA_PAN_RATE: f32 = 1.0; // TODO: factor in screen ui scale and camera zoom
 
     let (mut camera_transform, action_state) = query.single_mut();
 
-    let activate_pan = action_state.pressed(&CameraMovement::ActivatePan);
-    if activate_pan {
-        let camera_pan_vector = action_state.axis_pair(&CameraMovement::Pan);
-        camera_transform.translation.x -= CAMERA_PAN_RATE * camera_pan_vector.x;
-        camera_transform.translation.y += CAMERA_PAN_RATE * camera_pan_vector.y;
-    }
+    let camera_pan_vector = action_state.axis_pair(&CameraMovement::Pan);
+    camera_transform.translation.x -= CAMERA_PAN_RATE * camera_pan_vector.x;
+    camera_transform.translation.y += CAMERA_PAN_RATE * camera_pan_vector.y;
 }
 
 fn zoom(
     mut query: Query<(&mut OrthographicProjection, &ActionState<CameraMovement>), With<Camera2d>>,
 ) {
+    const CAMERA_ZOOM_RATE: f32 = 0.005;
+    let (mut camera_projection, action_state) = query.single_mut();
+
+    if let Some(mouse_y) = action_state.axis_data(&CameraMovement::Zoom) {
+        camera_projection.scale *= 1.0 - (mouse_y.value * CAMERA_ZOOM_RATE);
+    }
+}
+
+fn zoom_scroll(
+    mut query: Query<(&mut OrthographicProjection, &ActionState<CameraMovement>), With<Camera2d>>,
+) {
     const CAMERA_ZOOM_RATE: f32 = 0.05;
 
     let (mut camera_projection, action_state) = query.single_mut();
-    let zoom_delta = action_state.value(&CameraMovement::Zoom);
+    let zoom_delta = action_state.value(&CameraMovement::ZoomWheel);
 
     camera_projection.scale *= 1. - zoom_delta * CAMERA_ZOOM_RATE;
 }
