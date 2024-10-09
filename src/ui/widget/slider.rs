e28637488d40b44abd7fc67c2e730e5c3d985de3
bokubeam <bokubeam@gmail.com>
2024-10-09 15:33:46 -0600

    Clamp knob to slider correctly

--- src/ui/widget/slider.rs
+++ src/ui/widget/slider.rs
@@ -9,6 +9,8 @@ use crate::ui::interaction::{OnDrag, OnResourceUpdated, WatchResource};
 use crate::ui::theme::*;
 use crate::ui::widget::Spawn;
 
+pub const KNOB_WIDTH: f32 = 12.0;
+
 pub trait SliderWidget {
     fn slider<R: Resource + std::fmt::Debug + From<f32> + Into<f32> + Copy + Clone>(
         &mut self,
@@ -73,8 +75,8 @@ impl<T: Spawn> SliderWidget for T {
                         Name::new("Slider Left Bound"),
                         NodeBundle {
                             style: Style {
-                                width: Px(5.),
-                                height: Px(5.),
+                                width: Px(1.),
+                                height: Px(1.),
                                 ..default()
                             },
                             background_color: BackgroundColor(WHITE.into()),
@@ -88,8 +90,8 @@ impl<T: Spawn> SliderWidget for T {
                         ButtonBundle {
                             style: Style {
                                 position_type: PositionType::Absolute,
-                                width: Px(12.0),
-                                height: Px(12.0),
+                                width: Px(KNOB_WIDTH),
+                                height: Px(KNOB_WIDTH),
                                 border: UiRect::all(Px(0.5)),
                                 ..default()
                             },
@@ -110,8 +112,8 @@ impl<T: Spawn> SliderWidget for T {
                         Name::new("Slider Right Bound"),
                         NodeBundle {
                             style: Style {
-                                width: Px(5.),
-                                height: Px(5.),
+                                width: Px(1.),
+                                height: Px(1.),
                                 ..default()
                             },
                             background_color: BackgroundColor(WHITE.into()),
@@ -141,17 +143,28 @@ pub struct SliderRightBound;
 fn update_knob_position<R: Resource + std::fmt::Debug + Into<f32> + Copy + Clone>(
     trigger: Trigger<OnResourceUpdated<R>>,
     resource: Res<R>,
-    slot_q: Query<(&GlobalTransform, &Children), With<SliderSlot>>,
     mut knob_q: Query<(&mut Style, &Parent), With<SliderKnob>>,
+    slot_q: Query<&Children, With<SliderSlot>>,
     left_bound_q: Query<&GlobalTransform, With<SliderLeftBound>>,
     right_bound_q: Query<&GlobalTransform, With<SliderRightBound>>,
 ) -> Result<()> {
-    let (mut knob_style, knob_parent) = knob_q.get_mut(trigger.entity())?;
-    let x: f32 = (*resource).into();
+    let (mut knob_style, parent) = knob_q.get_mut(trigger.entity())?;
+    let resource_value: f32 = (*resource).into();
+    let slot_children = slot_q.get(parent.get()).unwrap();
+    let left_bound: f32 = slot_children
+        .iter()
+        .find_map(|child| left_bound_q.get(*child).ok())
+        .map(|gt| gt.translation().x)
+        .ok_or(Error::Custom("Slider missing a left bound.".into()))?;
+    let right_bound: f32 = slot_children
+        .iter()
+        .find_map(|child| right_bound_q.get(*child).ok())
+        .map(|gt| gt.translation().x)
+        .ok_or(Error::Custom("Slider missing a right bound.".into()))?;
 
-    let percentage = get_percentage(1.0, 200.0, x) * 100.0;
-
-    knob_style.left = Percent(percentage);
+    let percentage = get_percentage(1.0, 200.0, resource_value);
+    let knob_x: f32 = (0.0).lerp(right_bound - left_bound, percentage) - (KNOB_WIDTH * 0.5);
+    knob_style.left = Px(knob_x);
 
     Ok(())
 }
@@ -161,12 +174,12 @@ fn on_drag<R: Resource + std::fmt::Debug + From<f32> + Copy + Clone>(
     mut resource: ResMut<R>,
     mouse_data: Res<MouseData>,
     knob_q: Query<&Parent, With<SliderKnob>>,
-    slot_q: Query<(&GlobalTransform, &Children), With<SliderSlot>>,
+    slot_q: Query<&Children, With<SliderSlot>>,
     left_bound_q: Query<&GlobalTransform, With<SliderLeftBound>>,
     right_bound_q: Query<&GlobalTransform, With<SliderRightBound>>,
 ) -> Result<()> {
     let parent = knob_q.get(trigger.entity()).unwrap();
-    let (slot_transform, slot_children) = slot_q.get(parent.get()).unwrap();
+    let slot_children = slot_q.get(parent.get()).unwrap();
     let left_bound: f32 = slot_children
         .iter()
         .find_map(|child| left_bound_q.get(*child).ok())
@@ -185,12 +198,6 @@ fn on_drag<R: Resource + std::fmt::Debug + From<f32> + Copy + Clone>(
 
     Ok(())
 }
-
-// Scale the value in the range of the slider
-// Value has a min and max
-// Slider has a size in Px
-// Get percentage of position on the slider and map
-// to percentage of range for the value
 
 fn get_percentage(min: f32, max: f32, value: f32) -> f32 {
     ((value - min) / (max - min)).clamp(0.0, 1.0)
