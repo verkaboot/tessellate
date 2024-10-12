17574fd110e38858b1d0fee92d1630762c28fb71
bokubeam <bokubeam@gmail.com>
2024-10-12 14:14:30 -0600

    Split up main vs lib plugin code

--- src/lib.rs
+++ src/lib.rs
@@ -2,54 +2,20 @@ mod background;
 mod camera;
 mod screen;
 
-use bevy::{
-    asset::load_internal_binary_asset, prelude::*, window::WindowResolution, winit::WinitSettings,
-};
-use bevy_framepace::{FramepaceSettings, Limiter};
-use bevy_inspector_egui::quick::WorldInspectorPlugin;
+use bevy::prelude::*;
 
 pub struct AppPlugin;
 
 impl Plugin for AppPlugin {
     fn build(&self, app: &mut App) {
-        app
-            // Bevy Plugin Setup
-            .add_plugins(
-                DefaultPlugins
-                    .set(WindowPlugin {
-                        primary_window: Some(Window {
-                            title: "Animaboot".into(),
-                            resolution: WindowResolution::new(1920., 1080.)
-                                .with_scale_factor_override(1.0),
-                            present_mode: bevy::window::PresentMode::AutoNoVsync,
-                            ..default()
-                        }),
-                        ..default()
-                    })
-                    .set(ImagePlugin::default_nearest()),
-            )
-            .insert_resource(WinitSettings::desktop_app())
-            // 3rd Party Plugins
-            .add_plugins(bevy_framepace::FramepacePlugin)
-            .insert_resource(FramepaceSettings {
-                limiter: Limiter::from_framerate(144.0),
-            })
-            .add_plugins(WorldInspectorPlugin::new())
-            // App Plugins
-            .add_plugins((
-                camera::plugin,
-                canvas::plugin,
-                background::plugin,
-                ui::plugin,
-            ));
+        // Workspace Plugins
+        app.add_plugins((
+            camera::plugin,
+            canvas::plugin,
+            background::plugin,
+            ui::plugin,
+        ));
 
         app.add_systems(Startup, screen::setup);
-
-        load_internal_binary_asset!(
-            app,
-            TextStyle::default().font,
-            "../assets/fonts/NotoSans.ttf",
-            |bytes: &[u8], _path: String| { Font::try_from_bytes(bytes.to_vec()).unwrap() }
-        );
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,43 @@
 #![cfg_attr(not(feature = "dev"), windows_subsystem = "windows")]
 
 use animaboot::AppPlugin;
-use bevy::prelude::*;
+use bevy::{asset::load_internal_binary_asset, prelude::*};
+use bevy::{window::WindowResolution, winit::WinitSettings};
+use bevy_framepace::{FramepaceSettings, Limiter};
+use bevy_inspector_egui::quick::WorldInspectorPlugin;
 
 fn main() -> AppExit {
-    App::new().add_plugins(AppPlugin).run()
+    let mut app = App::new();
+
+    // Bevy Plugin Setup
+    app.add_plugins(
+        DefaultPlugins
+            .set(WindowPlugin {
+                primary_window: Some(Window {
+                    title: "Animaboot".into(),
+                    resolution: WindowResolution::new(1920., 1080.).with_scale_factor_override(1.0),
+                    present_mode: bevy::window::PresentMode::AutoNoVsync,
+                    ..default()
+                }),
+                ..default()
+            })
+            .set(ImagePlugin::default_nearest()),
+    )
+    .insert_resource(WinitSettings::desktop_app())
+    // 3rd Party Plugins
+    .add_plugins(bevy_framepace::FramepacePlugin)
+    .insert_resource(FramepaceSettings {
+        limiter: Limiter::from_framerate(144.0),
+    })
+    .add_plugins(WorldInspectorPlugin::new());
+
+    // Build default font into the binary
+    load_internal_binary_asset!(
+        app,
+        TextStyle::default().font,
+        "../assets/fonts/NotoSans.ttf",
+        |bytes: &[u8], _path: String| { Font::try_from_bytes(bytes.to_vec()).unwrap() }
+    );
+
+    app.add_plugins(AppPlugin).run()
 }
